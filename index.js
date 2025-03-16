264369a4a9ba970c2cc0e34306af6ed0d49de175
Nilesh Prajapat <158799822+Nilesh-Prajapat@users.noreply.github.com>
2025-03-16 15:26:02 +0530

    cors fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
   // CORS headers to allow specific origin and methods
   const FRONTEND_URL = process.env.FRONTEND_URL; // Set your frontend URL in the .env file
 
-  res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL); // Set your frontend origin here
+  res.setHeader('Access-Control-Allow-Origin', "*"); // Set your frontend origin here
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
